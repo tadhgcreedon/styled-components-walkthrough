5722c86cee9827a088177ba2b94038ca6d27dcee
Tadhg Creedon <tadhg.creedon@rangle.io>
2018-09-07 13:57:57 -0400

    change some other stuff

--- src/components/RightPanel/right-panel.style.js
+++ src/components/RightPanel/right-panel.style.js
@@ -2,11 +2,9 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   position: fixed;
-  bottom: 0px;
   display: flex;
   flex-direction: column;
-  justify-content: flex-end;
-  height: 100%;
   margin: 0 auto;
   justify-content: space-between;
+  padding: 16px;
 `;
